a56924207f7fd14ab31a5f29755fa94076ca0ee4
Hussam <o.hussam@gmail.com>
2022-08-06 16:51:23 +0300

    Creating user porfile and migrate the chanes.

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,46 @@
+import email
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 # Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for users' profiles """
+    def creatre_user(self, name, email, password=None):
+        """Create new user profile"""
+        if not email:
+            raise ValueError("User must have an email address.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, name, email, password):
+        user = self.creatre_user(name=name, email=email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Class for Mapping User to Database"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    objects = UserProfileManager()
+
+    def get_full_name(self):
+        """Return User Full Name"""
+        return self.name
+
+    def get_short_name(self):
+        """Return User Short Name"""
+        return self.name
+    
